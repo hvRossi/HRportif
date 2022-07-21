ad20ca92a3c71053131c664809b6c43f097cae81
hvRossi <hugo.vrossi@gmail.com>
2022-07-20 22:51:26 -0300

    changes

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -19,7 +19,7 @@ window.addEventListener('load', () => {
 
         const task_el_content = document.createElement("div");
         task_el_content.classList.add("content");
-        task_el_content.innerText = task;
+        // task_el_content.innerText = task;
 
         task_el.appendChild(task_el_content);
 
@@ -29,23 +29,45 @@ window.addEventListener('load', () => {
         task_input_el.value = task;
         task_input_el.setAttribute("readonly", "readonly");
 
-        // task_el_content.appendChild(task_input_el);
+        task_el_content.appendChild(task_input_el);
         // esse se adcionar, tem que tirar a linha 22
 
         const task_actions_el = document.createElement("div");
         task_actions_el.classList.add("actions");
 
-        const viviani = document.createElement("button");
-        
+        const task_edit_el = document.createElement("button");
+        task_edit_el.classList.add("edit");
+        task_edit_el.innerHTML="Edit";
 
+        const task_delete_el = document.createElement("button");
+        task_delete_el.classList.add("delete");
+        task_delete_el.innerHTML="delete";
+
+        task_el.appendChild(task_actions_el);
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
 
         list_el.appendChild(task_el);
 
         input.value = "";
 
+        task_edit_el.addEventListener('click', () => {
+            if (task_edit_el.innerText.toLowerCase() == "edit") {
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+                task_edit_el.innerText = "Save";
+            } 
+            else {
+                task_input_el.setAttribute("readonly", "readonly");
+                task_edit_el.innerText = "Edit";
 
+            }
+
+
+    
+        });
+        task_delete_el.addEventListener('click', () => { 
+            list_el.removeChild(task_el);
+        });
     })
-
-   
-
 })
