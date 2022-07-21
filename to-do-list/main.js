33f6f1ea6857f51ccda70300c5612507ffd81b7f
hvRossi <hugo.vrossi@gmail.com>
2022-07-20 23:13:28 -0300

    changs

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -1,10 +1,11 @@
-alert("Repeito é pra quem tem! pra em tem! pra quem tem!")
+
 
 window.addEventListener('load', () => {
 
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
+    alert("Repeito é pra quem tem! pra em tem! pra quem tem!")
 
 
     
